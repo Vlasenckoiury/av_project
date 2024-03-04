fe63eba067630dc9ca385ac319178e977b407bd1
iuryvlasenko <vlasencko.iury1999@yandex.by>
2024-03-04 12:33:18 +0300

    обновление адреса

--- av_backend/av_backend/urls.py
+++ av_backend/av_backend/urls.py
@@ -8,7 +8,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('av_car.urls')),
-    path('', include(CarListCreate.as_view()), name='list_car'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
