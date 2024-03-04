5bd8c55d6d285c7b5335314deac6c86349491d94
iuryvlasenko <vlasencko.iury1999@yandex.by>
2024-03-04 11:56:05 +0300

    бот в Telegram первые команды

---
+++ av_backend/av_car/main_bot.py
@@ -0,0 +1,16 @@
+from telebot.async_telebot import AsyncTeleBot
+import aiohttp
+
+bot = AsyncTeleBot('6613313841:AAEyd57MKTPC-pL13saMn5sDl2pxFzTf3Hk')
+
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+async def send_welcome(message):
+    await bot.reply_to(message, """Добро пожаловать в Av Car""")
+
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+async def echo_message(message):
+    await bot.reply_to(message, message.text)
