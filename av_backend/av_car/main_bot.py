bc031692557a1a2f04bc5393bf737af21a10fae5
iuryvlasenko <vlasencko.iury1999@yandex.by>
2024-03-04 12:33:42 +0300

    обновление сообщений

--- av_backend/av_car/main_bot.py
+++ av_backend/av_car/main_bot.py
@@ -7,7 +7,7 @@ bot = AsyncTeleBot('6613313841:AAEyd57MKTPC-pL13saMn5sDl2pxFzTf3Hk')
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
 async def send_welcome(message):
-    await bot.reply_to(message, """Добро пожаловать в Av Car""")
+    await bot.send_message(message.chat.id, """Добро пожаловать в Av Car""")
 
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
