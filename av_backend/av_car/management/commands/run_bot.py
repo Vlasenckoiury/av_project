c87f51d98c1075cc6b9e2270e236e860d5e86589
iuryvlasenko <vlasencko.iury1999@yandex.by>
2024-03-05 01:50:55 +0300

    добавление настройки для бота разрешения всех каналов

--- av_backend/av_car/management/commands/run_bot.py
+++ av_backend/av_car/management/commands/run_bot.py
@@ -3,15 +3,18 @@ import logging
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
+from telebot import util
 
 from av_project.av_backend.av_car.main_bot import bot
 
 logger = logging.getLogger(__name__)
+
+
 class Command(BaseCommand):
     help = "Запуск бота"
 
     def handle(self, *args, **options):
         try:
-            asyncio.run(bot.infinity_polling(logger_level=settings.LOG_LEVEL))
+            asyncio.run(bot.infinity_polling(logger_level=settings.LOG_LEVEL, allowed_updates=util.update_types))
         except Exception as err:
             logger.error(f'Ошибка: {err}')
