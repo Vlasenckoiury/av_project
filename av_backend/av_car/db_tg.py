af6ee3c9b4eecec78ca007cdb0d24d8606b6074e
iuryvlasenko <vlasencko.iury1999@yandex.by>
2024-03-14 04:21:55 +0300

    добавление запросов к бд

--- av_backend/av_car/db_tg.py
+++ av_backend/av_car/db_tg.py
@@ -1,7 +1,10 @@
+## -*- coding: utf-8 -*-
+import logging
+
 import psycopg2
 from functools import wraps
 
-from av_project.av_backend.av_car.main_bot import *
+# from av_project.av_backend.av_car.main_bot import *
 
 
 def db_connection(func):
@@ -55,3 +58,25 @@ def get_contact(cursor, message):
     cursor.execute(update_query, (message.contact.phone_number, ))
     print(f"Значение в колонке {message.contact.phone_number} пользователя с telegram_id {message.chat.id} успешно обновлено в базе данных")
 
+
+@db_connection
+def get_time(cursor):
+    # cursor.execute("SELECT start_time FROM av_car_newmessage WHERE created_at >= NOW() - INTERVAL '1 day'")
+    cursor.execute("SELECT start_time FROM av_car_newmessage")
+    new_records = cursor.fetchall()
+    return new_records
+
+
+@db_connection
+def get_channel(cursor):
+    try:
+        cursor.execute("""
+        SELECT av_car_telegramchat.chat_link, av_car_newmessage.message
+        FROM av_car_newmessage
+        INNER JOIN av_car_telegramchat ON av_car_newmessage.link_chat_id = av_car_telegramchat.id;
+        """)
+        # cursor.execute("SELECT link_chat_id, message FROM av_car_newmessage")
+        rows = cursor.fetchall()
+        return rows
+    except Exception as err:
+        logging.info(f'Данные не получены {err}')
