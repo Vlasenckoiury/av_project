f9c5c1489bda11f46a15c08bdaa352ec5a294a39
iuryvlasenko <vlasencko.iury1999@yandex.by>
2024-03-16 15:24:04 +0300

    команда запуска бота

--- av_backend/av_car/management/commands/run_bot.py
+++ av_backend/av_car/management/commands/run_bot.py
@@ -16,6 +16,6 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         try:
             # asyncio.run(bot.infinity_polling(logger_level=settings.LOG_LEVEL, allowed_updates=util.update_types))
-            bot.infinity_polling(none_stop=True)
+            bot.infinity_polling()
         except Exception as err:
             logger.error(f'Ошибка: {err}')
